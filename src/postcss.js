f803f8599c1e9f9849f0b1ce754a43fbfdabdcae
Tom Lawton <tom.lawton@cndu.it>
2023-01-31 21:52:09 +0000

    PostCSS 8 and decls

--- src/postcss.js
+++ src/postcss.js
@@ -6,32 +6,39 @@
 const postcss = require('postcss');
 const store = require('./store');
 const normalize = require('./utils/normalize');
+const processed = Symbol('wpe-processed')
 
-module.exports = postcss.plugin('WpEditorPostCSS', options => {
+module.exports = (options) => {
 
-    function addToStore(name, atRule) {
-
-        const css = postcss.root().append(atRule.nodes).toString();
-        const query = atRule.params;
-        
-        store.addMedia(name, css, options.path, query);
+    function addToStore(name, node) {
+        const css = postcss.root().append(node.type === 'atrule' ? node.nodes : node).toString();
+        store.addMedia(name, css, options.path, '');
     }
 
-    return (css, result) => {
-
-        css.walkAtRules('editor', atRule => {
+    return { 
+        postcssPlugin: "WpEditorPostCSS",
+        AtRule(atRule) {
+        if(atRule.name == 'editor' || atRule.name === 'editor-only') {
+          if (!atRule[processed]) {
             const name = atRule.params ? atRule.params : 'default';
+            
+            if(atRule.nodes.length > 0 && atRule.nodes[0].type == 'decl') {
+                const ghostNode = new postcss.Rule({selector: atRule.parent.selector})
+                ghostNode.append(atRule.clone().nodes)
+                atRule.replaceWith(atRule.nodes);
+                atRule.remove();
+                addToStore(name, ghostNode.clone());
 
-            addToStore(name, atRule.clone());
-            atRule.replaceWith(atRule.nodes);
-            atRule.remove();
-        });
-
-        css.walkAtRules('editor-only', atRule => {
-            const name = atRule.params ? atRule.params : 'default';
-
-            addToStore(name, atRule.clone());
-            atRule.remove();
-        });
-    };
-});
+            } else {
+                addToStore(name, atRule.clone());
+                if(atRule.name === 'editor') {
+                    atRule.replaceWith(atRule.nodes);
+                }
+                atRule.remove();
+            }
+            atRule[processed] = true
+          }
+        }
+        
+      }}
+};
