dd1d52850ee929065744e215b449886b0a162ed0
Tom Lawton <tom.lawton@cndu.it>
2023-01-31 17:20:22 +0000

    Extract @editor and @editor-only rules

--- src/postcss.js
+++ src/postcss.js
@@ -7,58 +7,31 @@ const postcss = require('postcss');
 const store = require('./store');
 const normalize = require('./utils/normalize');
 
-module.exports = postcss.plugin('MediaQueryPostCSS', options => {
+module.exports = postcss.plugin('WpEditorPostCSS', options => {
 
     function addToStore(name, atRule) {
 
-        const css = postcss.root().append(atRule).toString();
+        const css = postcss.root().append(atRule.nodes).toString();
         const query = atRule.params;
         
         store.addMedia(name, css, options.path, query);
     }
 
-    function getGroupName(name) {
-        const groupNames = Object.keys(options.groups);
-
-        for (let i = 0; i < groupNames.length; i++) {
-            const groupName = groupNames[i];
-            const group = options.groups[groupName];
-
-            if (group instanceof RegExp) {
-                if (name.match(group)) {
-                    return groupName;
-                }
-            } else if (Array.isArray(group)) {
-                if (group.includes(name)) {
-                    return groupName;
-                }
-            }
-        }
-    }
-
-    function getQueryName(query) {
-        const queries = Object.keys(options.queries);
-
-        for (let i = 0; i < queries.length; i++) {
-            if (normalize(query) === normalize(queries[i])) {
-                return options.queries[queries[i]];
-            }
-        }
-    }
-
     return (css, result) => {
 
-        css.walkAtRules('media', atRule => {
+        css.walkAtRules('editor', atRule => {
+            const name = atRule.params ? atRule.params : 'default';
 
-            const queryname = getQueryName(atRule.params);
+            addToStore(name, atRule);
+            atRule.replaceWith(atRule.nodes);
+            atRule.remove();
+        });
 
-            if (queryname) {
-                const groupName = getGroupName(options.basename);
-                const name = groupName ? `${groupName}-${queryname}` : `${options.basename}-${queryname}`;
-                
-                addToStore(name, atRule);
-                atRule.remove();
-            }
+        css.walkAtRules('editor-only', atRule => {
+            const name = atRule.params ? atRule.params : 'default';
+
+            addToStore(name, atRule);
+            atRule.remove();
         });
     };
 });
